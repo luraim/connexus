0903eae6d3a1aee9e807cbebba089148194c243e
muralikp <muralikp@gmail.com>
2020-03-24 12:41:51 -0700

    simple page server

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,9 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+object Versions {
+    const val ktor = "1.3.2"
+}
 
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin.
@@ -12,8 +17,19 @@ repositories {
     // Use jcenter for resolving dependencies.
     // You can declare any Maven/Ivy/file repository here.
     jcenter()
+    mavenCentral()
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType<KotlinCompile>().all() {
+    kotlinOptions {
+        jvmTarget = "1.8"
+        //freeCompilerArgs = listOf("-Xjsr305=strict")
+    }
+}
 
 dependencies {
     // Align versions of all Kotlin components
@@ -21,6 +37,9 @@ dependencies {
 
     // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("io.ktor:ktor-server-netty:${Versions.ktor}")
+    implementation("io.github.microutils:kotlin-logging:1.7.9")
+    implementation("ch.qos.logback:logback-classic:1.2.3")
 
     // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
@@ -33,6 +52,7 @@ application {
     // Define the main class for the application.
     mainClassName = "connexus.AppKt"
 }
+
 
 tasks.withType<Jar> {
     manifest {
