7cda6ba156977d8def364f79cbb3772ba8be6e5c
muralikp <muralikp@gmail.com>
2020-05-13 16:23:32 -0700

    wip

--- server/pageList.go
+++ server/pageList.go
@@ -35,16 +35,32 @@ func (sr *Server) pageListHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	sort.Strings(topicNames)
 
+	topics := make([]*TopicDetails, 0)
+	for _, t := range topicNames {
+		topics = append(topics, &TopicDetails{
+			Name:     t,
+			Incoming: len(sr.incomingLinks(t)),
+			Outgoing: len(sr.outgoingLinks(t)),
+			Exists:   sr.PageExists(t),
+		})
+	}
+
 	err = sr.pageListTemplate.ExecuteTemplate(w, "pageList.html",
-		TopicList{sr.homeTopic, topicNames, sr})
+		TopicList{sr.homeTopic, topics})
 	if err != nil {
 		httpErr(w, err)
 		return
 	}
 }
 
+type TopicDetails struct {
+	Name     string
+	Incoming int
+	Outgoing int
+	Exists   bool
+}
+
 type TopicList struct {
 	HomePage string
-	Names    []string
-	Server   *Server
+	Topics   []*TopicDetails
 }
